880d0a5f339cbe2173c3f4d4d49a4e8ffa87416b
yubin <shin.yubin18@gmail.com>
2023-08-17 21:49:36 +0900

    Fix : 유저정보 수정 코드 작성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,47 @@
-import { Body, Controller, Get, Patch, Post, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  NotFoundException,
+  Patch,
+  Post,
+  UnprocessableEntityException,
+  UseGuards,
+} from "@nestjs/common";
 import { User } from "@prisma/client";
 import { GetUser } from "../auth/decorator";
 import { JwtGuard } from "../auth/guard";
 import { UsersService } from "./users.service";
 import { EditUserDto } from "./dto";
 import { ApiTags } from "@nestjs/swagger";
+import { validate } from "class-validator";
 @ApiTags("users")
 @UseGuards(JwtGuard)
 @Controller("users")
 export class UsersController {
   constructor(private userService: UsersService) {}
+
   @Get("me")
   getMe(@GetUser() user: User) {
     return user;
   }
+
   @Patch("me")
-  editUser(@GetUser("user_id") userId: number, @Body() dto: EditUserDto) {
-    return this.userService.editUser(userId, dto);
+  async editUser(@GetUser("user_id") userId: number, @Body() dto: EditUserDto) {
+    // 유효성 검사 수행
+    const errors = await validate(dto);
+    if (errors.length > 0) {
+      throw new UnprocessableEntityException(errors);
+    }
+    try {
+      const result = await this.userService.editUser(userId, dto);
+      return result;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
