f659b11422d2871e4067433651e7c02b83127248
yubin <shin.yubin18@gmail.com>
2023-08-25 20:32:31 +0900

    Feat : places/search 반환모델 생성

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -17,6 +17,11 @@ import {
   ApiResponse,
 } from "@nestjs/swagger";
 import { PlacesService } from "./places.service"; // PlacesService의 경로로 수정해야 합니다.
+import {
+  SearchResponsesArray,
+  SearchResponse,
+  PlaceCategory,
+} from "./responses"; // 'your-models'는 실제 모델 파일의 경로에 맞게 변경해주세요.
 
 @ApiTags("places")
 @Controller("places")
@@ -26,7 +31,8 @@ export class PlacesController {
   @Get("/search")
   @ApiResponse({
     status: 200,
-    description: "The record has been successfully created.",
+    description: "",
+    type: SearchResponsesArray, // 반환 모델을 지정
   })
   @ApiResponse({ status: 404, description: "해당 장소 검색 실패" })
   async getPlaceSearch(
@@ -36,12 +42,18 @@ export class PlacesController {
     @Res() res
   ): Promise<void> {
     try {
-      const searchResult = await this.placesService.findPlacesInfoFromKakao(
-        query,
-        xCoordinate,
-        yCoordinate
-      );
-      res.status(HttpStatus.OK).json(searchResult);
+      const searchResult: SearchResponse[] =
+        await this.placesService.findPlacesInfoFromKakao(
+          query,
+          xCoordinate,
+          yCoordinate
+        );
+
+      const responsesArray: SearchResponsesArray = {
+        responses: searchResult,
+      };
+
+      res.status(HttpStatus.OK).json(responsesArray);
     } catch (error) {
       res
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -63,55 +75,4 @@ export class PlacesController {
     const result = await this.placesService.getOne(placeId);
     return result;
   }
-
-  // @Get("/search")
-  // async getPlaceSearch(@Query("query") query: string): Promise<any> {
-  //   try {
-  //     const searchResult = await this.placesService.findPlaceInfoFromKakao(
-  //       query
-  //     );
-  //     return searchResult;
-  //   } catch (error) {
-  //     throw new NotFoundException("해당 장소를 찾을 수 없습니다.");
-  //   }
-  // }
-
-  // @Get("/search/coordinate")
-  // async getPlaceSearchWitCoordinates(
-  //   @Query("query") query: string,
-  //   @Query("x") xCoordinate?: number, // 선택적 파라미터
-  //   @Query("y") yCoordinate?: number // 선택적 파라미터
-  // ): Promise<any> {
-  //   try {
-  //     const searchResult = await this.placesService.findPlaceInfoFromKakao(
-  //       query,
-  //       xCoordinate,
-  //       yCoordinate
-  //     );
-  //     return searchResult;
-  //   } catch (error) {
-  //     throw new NotFoundException("해당 장소를 찾을 수 없습니다.");
-  //   }
-  // }
-
-  // @Get("/init")
-  // async initLandmarks(
-  //   @Query("place_name") place_name: string,
-  //   @Query("place_latitude") place_latitude: number,
-  //   @Query("place_longitude") place_longitude: number,
-  //   @Res() res
-  // ): Promise<void> {
-  //   try {
-  //     const result = await this.placesService.createPlace(
-  //       place_name,
-  //       place_latitude,
-  //       place_longitude
-  //     );
-  //     res.status(HttpStatus.OK).json(result);
-  //   } catch (error) {
-  //     res
-  //       .status(HttpStatus.INTERNAL_SERVER_ERROR)
-  //       .json({ message: "Error occurred during init." });
-  //   }
-  // }
 }
