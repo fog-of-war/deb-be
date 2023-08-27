91bf134eccec67eb9e90dd11f3606256d1ed1a84
root <root@yubin>
2023-08-27 18:05:46 +0900

    Fix : post 시 업데이트된 레벨과 뱃지 정보 응답모델 보완

--- src/posts/responses/post.posts.response.ts
+++ src/posts/responses/post.posts.response.ts
@@ -1,33 +1,37 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 class BadgeInfo {
-  @ApiProperty()
+  @ApiProperty({ description: '뱃지 ID' })
   badge_id: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: '뱃지 이름' })
   badge_name: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: '뱃지 카테고리' })
   badge_category_id: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: '뱃지 획득 조건' })
   badge_criteria: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: '뱃지 획득 시 부여 포인트' })
   badge_points: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: '뱃지를 획득한 사용자 ID' })
   badge_user_id: number;
+}
 
-  constructor(data: Partial<BadgeInfo>) {
-    Object.assign(this, data);
-  }
+export class StateInfo {
+  @ApiProperty({ description: '게시글 작성 후 레벨업' })
+  new_level?: number;
+  
+  @ApiProperty({
+    description: '게시글 작성 후 신규 뱃지 획득',
+    type: [BadgeInfo], // 이 부분을 추가하여 뱃지 배열임을 명시
+  })
+  new_badges?: BadgeInfo[];
 }
 
 export class PostPostsResponse {
-  @ApiProperty()
-  state: {
-    new_level?: number;
-    new_badges?: BadgeInfo[];
-  };
+  @ApiProperty({ description: 'state 정보' })
+  state?: StateInfo;
 }
--- src/ranks/ranks.controller.ts
+++ src/ranks/ranks.controller.ts
@@ -4,7 +4,7 @@ import { CreateRankDto } from './dto/create-rank.dto';
 import { UpdateRankDto } from './dto/update-rank.dto';
 import { GetUser } from "../auth/decorator";
 import { JwtGuard } from 'src/auth/guard';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetOneRankResponse,GetManyRanksResponse } from './responses';
 
 @ApiTags("ranks")
@@ -29,6 +29,7 @@ export class RanksController {
     description: "",
     type: GetOneRankResponse, // 반환 모델을 지정
   })
+  @ApiBearerAuth("access_token")
   @UseGuards(JwtGuard)
   async getRankByUserId(@GetUser("user_id") userId: number) {
     const result = await this.ranksService.getUserRank(userId);
