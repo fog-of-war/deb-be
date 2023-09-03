07d1bd389f6ceed3ab831340896537e9ae043548
yubin <shin.yubin18@gmail.com>
2023-09-03 21:20:55 +0900

    Feat : 카카오 로그인 요청 미도착 확인중

--- src/auth/strategy/kakao.strategy.ts
+++ src/auth/strategy/kakao.strategy.ts
@@ -6,6 +6,7 @@ import { UsersService } from "../../users/users.service";
 import { PrismaService } from "../../prisma/prisma.service";
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy, "kakao") {
+  httpService: any;
   constructor(
     private readonly config: ConfigService,
     private prisma: PrismaService,
@@ -15,6 +16,7 @@ export class KakaoStrategy extends PassportStrategy(Strategy, "kakao") {
       clientID: config.get("KAKAO_CLIENT_ID"),
       clientSecret: "",
       callbackURL: config.get("KAKAO_REDIRECT_URL"),
+      scope: ["account_email", "profile_nickname"],
     });
   }
 
@@ -24,8 +26,46 @@ export class KakaoStrategy extends PassportStrategy(Strategy, "kakao") {
     profile: Profile,
     done: (error: any, user?: any, info?: any) => void
   ) {
+    // const { id, email } = profileInfo;
+
     try {
-      const { _json } = profile;
+      console.log(
+        "🚀 ~ file: kakao.strategy.ts:37 ~ KakaoStrategy ~ classKakaoStrategyextendsPassportStrategy ~ accessToken:",
+        accessToken
+      );
+
+      const options = {
+        url: `https://kauth.kakao.com/oauth/authorize`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          params: {
+            client_id: this.config.get("KAKAO_CLIENT_ID"),
+            redirect_uri: this.config.get("KAKAO_REDIRECT_URL"),
+            response_type: "code",
+            scope: "account_email",
+          },
+        },
+      };
+      // 요청 보내기
+      const response = await this.httpService
+        .get(options.url, {
+          headers: options.headers,
+        })
+        .toPromise();
+      const profileInfo = response.data.response;
+      console.log(
+        "🚀 ~ file: kakao.strategy.ts:51 ~ KakaoStrategy ~ classKakaoStrategyextendsPassportStrategy ~ response:",
+        response
+      );
+      console.log(
+        "🚀 ~ file: kakao.strategy.ts:48 ~ KakaoStrategy ~ classKakaoStrategyextendsPassportStrategy ~ profileInfo:",
+        profileInfo
+      );
+      const { _json } = profileInfo;
+      console.log(
+        "🚀 ~ file: kakao.strategy.ts:65 ~ KakaoStrategy ~ classKakaoStrategyextendsPassportStrategy ~ profileInfo:",
+        profileInfo
+      );
       const userDto = {
         user_providerId: "kakao",
         user_email: _json.kakao_account.email,
