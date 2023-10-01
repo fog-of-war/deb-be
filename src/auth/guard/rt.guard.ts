eacf503c07a36fd69d493376acde8ef06f372e7c
yubin <shin.yubin18@gmail.com>
2023-10-01 09:16:31 +0900

    Feat : 리프레시 토큰 전략명 맞추기

--- src/auth/guard/rt.guard.ts
+++ src/auth/guard/rt.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class RtGuard extends AuthGuard("rt") {
+export class RtGuard extends AuthGuard("jwt-refresh") {
   constructor() {
     super();
   }
