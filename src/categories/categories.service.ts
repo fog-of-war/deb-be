2e07b4ce4cc6b948b20fa8aca3e4c005fbf107cd
yubin <shin.yubin18@gmail.com>
2023-10-01 18:05:30 +0900

    Feat : 카테고리 서비스 정리

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -4,28 +4,13 @@ import { PrismaService } from "src/prisma/prisma.service";
 @Injectable()
 export class CategoriesService {
   constructor(private prisma: PrismaService) {}
-  async init() {
-    const categories = [
-      { category_name: "미식", category_score: 0 },
-      { category_name: "운동", category_score: 0 },
-      { category_name: "미술관", category_score: 0 },
-      { category_name: "역사", category_score: 0 },
-      { category_name: "커피", category_score: 0 },
-      { category_name: "기본", category_score: 0 },
-      { category_name: "랜드마크", category_score: 0 },
-      { category_name: "협찬", category_score: 0 },
-    ];
 
-    await this.prisma.category.createMany({
-      data: categories,
-      skipDuplicates: true,
-    });
-    return "This action adds a new category";
-  }
+  /** 카테고리 확인 시 사용 */
   async findCategoryName(category_id) {
     const result = await this.prisma.category.findFirst({
       where: { category_id: category_id },
     });
     return result;
   }
+  /** -------------------- */
 }
