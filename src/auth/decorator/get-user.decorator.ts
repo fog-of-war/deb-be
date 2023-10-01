f4310048ed9b46f085a233a39c4e208e7fd15315
yubin <shin.yubin18@gmail.com>
2023-10-01 16:27:25 +0900

    Feat : 배포 위하여 캐시모듈 설정 변경

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -3,10 +3,6 @@ import * as jwt from "jsonwebtoken";
 export const GetUser = createParamDecorator(
   (data: string | undefined, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    console.log(
-      "🚀 ~ file: get-current-user-id.decorator.ts:8 ~ request.user:",
-      request.user
-    );
 
     if (!data) return request.user;
     return request.user["sub"];
