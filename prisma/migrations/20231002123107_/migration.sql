3ae1be2f99d9d8e58dd0c403034957e557824aa1
root <root@yubin>
2023-10-13 17:46:19 +0900

    Fix : 프리즈마 migration 폴더 삭제

--- prisma/migrations/20231002123107_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Alert" DROP CONSTRAINT "Alert_alerted_user_id_fkey";
-
--- AlterTable
-ALTER TABLE "Alert" ALTER COLUMN "alerted_user_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Alert" ADD CONSTRAINT "Alert_alerted_user_id_fkey" FOREIGN KEY ("alerted_user_id") REFERENCES "User"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
